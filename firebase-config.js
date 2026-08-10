25bcc9b3c2055d7dcf84ab5706ffe49a760d8fc8
injesus0725-tech <injesus0725@gmail.com>
2026-08-10 13:55:36 +0900

    Add disabled Firebase config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+/* Firebase is intentionally disabled until the project-specific web config is entered. */
+window.StudyVillageFirebaseConfig = {
+  enabled: false,
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
