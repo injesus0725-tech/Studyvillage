9963f58ccdcf48135ca42f21ffdeaa3158648d19
injesus0725-tech <injesus0725@gmail.com>
2026-08-17 18:25:17 +0900

    Remove unused legacy exploration retirement shim

--- legacy-exploration-retirement.js
+++
@@ -1,6 +0,0 @@
-/* Retire the pre-study-menu exploration overlay without touching shared village/ranking layout code. */
-(()=>{
-  const legacyPanel=document.querySelector('.sv-expedition-panel');
-  if(legacyPanel)legacyPanel.remove();
-  document.documentElement.dataset.legacyExplorationRetired='true';
-})();
