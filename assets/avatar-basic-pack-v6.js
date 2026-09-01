be473c82d518d15fcc8750d68fe9ed68e7c3d2ec
injesus0725-tech <injesus0725@gmail.com>
2026-09-02 08:51:26 +0900

    feat: draw feminine casual wardrobe pack

---
+++ assets/avatar-basic-pack-v6.js
@@ -0,0 +1,9 @@
+/* StudyVillage avatar pack v6: simple readable casual pieces for the tiny classroom avatar. */
+(()=>{const r=window.StudyVillageAvatar;if(!r?.ASSETS)return;const f=b=>`<svg viewBox="0 0 96 144" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">${b}</svg>`;const s=b=>f(`<g stroke="#684b43" stroke-width="1.45" stroke-linecap="round" stroke-linejoin="round">${b}</g>`);Object.assign(r.ASSETS,{
+'hair-ribbon-pony':{svg:s('<path fill="#6b4331" d="M25 39Q22 11 48 8t23 31l-7-9-5 8-11-12-9 12-8-8-3 9z"/><path fill="#6b4331" d="M68 22q18-2 17 15-1 13-14 17l-5-9q13-4 7-15z"/><path fill="#c96f91" d="M68 21l-8-6 2 10-2 9 9-7 9 7-2-10 2-9z"/>')},
+'outfit-blouse-cream':{svg:s('<path fill="#f3ead9" d="M29 73l9-6h20l9 6-3 36H32z"/><path fill="#d89aae" d="M40 68l8 10 8-10-2 15H42z"/><circle fill="#b78e54" cx="48" cy="89" r="2"/><circle fill="#b78e54" cx="48" cy="99" r="2"/>')},
+'outfit-hoodie-mint':{svg:s('<path fill="#79b9a5" d="M28 74l9-7h22l9 7-3 35H31z"/><path fill="#4e8978" d="M37 68q11 15 22 0l4 7-15 10-15-10z"/><path fill="#e7d6a0" d="M47 84h2v21h-2z"/>')},
+'bottom-skirt-pleated':{svg:s('<path fill="#745a91" d="M31 101h34l7 25H24z"/><path fill="#5d4777" d="M31 101h34v6H31z"/><path fill="none" stroke="#aa93bf" d="M35 107l-4 17m13-17-1 18m10-18 1 18m7-18 4 17"/>')},
+'bottom-skirt-denim':{svg:s('<path fill="#4f7195" d="M31 101h34l5 24H26z"/><path fill="#365675" d="M31 101h34v6H31z"/><path fill="none" d="M48 106v18M34 111h8m12 0h8"/>')},
+'shoes-sneakers-pink':{svg:s('<path fill="#f7f1e8" d="M28 122h18v13H24q-5-5 4-13zm22 0h18q9 8 4 13H50z"/><path fill="#cf7894" d="M30 122h14v7H27zm23 0h14l3 7H53z"/><path fill="#8f5367" d="M24 132h22v4H23zm26 0h22v4H50z"/>')}
+});})();
